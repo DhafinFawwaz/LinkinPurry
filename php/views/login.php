2fe2e8b7b340b92ccb035ec8b94f462780c9f7bb
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-10-18 19:41:08 +0700

    feat: login styling

--- php/views/login.php
+++ php/views/login.php
@@ -2,26 +2,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Linkedin</title>
 
-    <!-- <link rel="stylesheet" href="/public/global.css"> -->
+    <link rel="stylesheet" href="/public/css/login.css">
 </head>
 <body>
-    <form action="/login" method="post">
 
-        <label for="email">Email:</label>
-        <input id="email" name="email" required="" type="email"  value="<?php if(isset($data["form"]["email"])) echo $data["form"]["email"] ?>"/>
-        <div><?php if(isset($data["error"]["email"])) echo $data["error"]["email"] ?></div>
+    <div class="app__container">
+        <header>
+            <a class="linkedin-logo" href="/" aria-label="LinkedIn">
+                <?php require_once 'views/component/linkedin-logo-medium.php'; ?>
+            </a>
+        </header>
 
-        <label for="password">Password:</label>
-        <input id="password" name="password" required="" type="password"  value="<?php if(isset($data["form"]["password"])) echo $data["form"]["password"] ?>"/>
-        <div><?php if(isset($data["error"]["password"])) echo $data["error"]["password"] ?></div>
+        <main class="app__content">
     
-        <button type="submit">Login</button>
-    </form>
-    <div>Don't have an account?</div>
-    <a href="/register/company">Register as Company</a>
-    <a href="/register/jobseeker">Register as jobseeker</a>
+            <div class="card">
+                <h1 class="content__title ">Login</h1>
+                <form method="post" class="login__form" action="/login">
+                    
+                    <div class="form__input">
+                        <input id="email" name="email" required="" autofocus="" type="text" value="<?php if(isset($data["form"]["email"])) echo $data["form"]["email"] ?>">
+                        <label for="email">Email atau telepon</label>
+                        <div class="error__label"><?php if(isset($data["error"]["email"])) echo $data["error"]["email"] ?></div>
+                    </div>
+
+                    <div class="form__input">
+                        <input id="password" name="password" required="" autofocus="" type="password" value="<?php if(isset($data["form"]["password"])) echo $data["form"]["password"] ?>">
+                        <label for="password">Password</label>
+                        <div class="error__label"><?php if(isset($data["error"]["password"])) echo $data["error"]["password"] ?></div>
+                    </div>
+                
+                    <div class="form__bottom">
+                        <button class="button1" data-litms-control-urn="login-submit" aria-label="Login" type="submit">Login </button>
+                    </div>
+            
+                </form>
+            </div>
+
+            <br>
+            <div class="register__container">
+                <span>Baru mengenal LinkedIn? <a href="/register" class="button3">Bergabung sekarang</a></span>
+                
+            </div>
+        </main>
+
+        <footer class="footer__base">
+            <div class="footer__base__wrapper">
+                <p class="copyright">
+                    <?php require_once 'views/component/linkedin-logo-small.php'; ?>
+                </p>
+                © 2024
+            </div>
+        </footer>
+      
+            
+    </div>
 
 </body>
 </html>
