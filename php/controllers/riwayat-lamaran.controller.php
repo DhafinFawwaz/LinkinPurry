95f6c3b3bac3b3c8bc12dc5c6aded802188c2271
NopalAul <mnaufal0605@gmail.com>
2024-10-18 21:39:47 +0700

    feat: retrieve riwayat lamaran

--- php/controllers/riwayat-lamaran.controller.php
+++ php/controllers/riwayat-lamaran.controller.php
@@ -1,7 +1,19 @@
 <?php
 require_once __DIR__ . "/../lib/controller.php";
+require_once __DIR__ . "/../models/lamaran.model.php";
+
 class RiwayatLamaranController extends Controller {
-    public function handle(){
-        return $this->view("riwayat-lamaran.php");
+
+    public function handle() {
+        $user = $_SESSION['user'];
+        $user_id = $user->id;
+
+        $riwayatLamaran = Lamaran::getRiwayatLamaranByUserId($user_id);
+
+        // var_dump($riwayatLamaran);
+
+        return $this->view("riwayat-lamaran.php", [
+            "riwayatLamaran" => $riwayatLamaran
+        ]);
     }
 }
