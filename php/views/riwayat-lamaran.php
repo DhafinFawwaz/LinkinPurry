1963191e2179c0f098439b80d05e8bd07fe02bfb
NopalAul <mnaufal0605@gmail.com>
2024-10-20 23:35:57 +0700

    feat: navbar styling on riwayat lamaran
    rename css due to kocak

--- php/views/riwayat-lamaran.php
+++ php/views/riwayat-lamaran.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Application History</title>
 
-    <link rel="stylesheet" href="../public/css/riwayat-lamaran.css">
+    <link rel="stylesheet" href="../public/css/riwayat-lamaran1.css">
 </head>
 <body>
+    <section id="navbar">
+        <?php include 'component/navbar.php'; ?>
+    </section>
+
     <div class="container">
         <h2>Job Application History</h2>
         
