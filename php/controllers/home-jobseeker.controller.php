5fac2152d8854a477853ca2dc1b50ba8f6d6b0c0
NopalAul <mnaufal0605@gmail.com>
2024-10-21 20:14:09 +0700

    fix: use a tag on job card

--- php/controllers/home-jobseeker.controller.php
+++ php/controllers/home-jobseeker.controller.php
@@ -48,7 +48,7 @@ class HomeJobseekerController extends Controller {
         if (isset($lowonganList) && !empty($lowonganList)) {
             foreach ($lowonganList as $lowongan) {
                 echo "
-                    <div class='job-card' onclick=\"window.location.href='/detail-lowongan-jobseeker?id={$lowongan['lowongan_id']}'\">
+                    <a class='job-card' href='/detail-lowongan-jobseeker?id={$lowongan['lowongan_id']}'\">
                         <div class='job-picture'>
                             <img src='../public/assets/company_profile.svg' alt='job-picture'>
                         </div>
@@ -57,7 +57,7 @@ class HomeJobseekerController extends Controller {
                             <p>{$lowongan['company_name']}</p>
                             <p class='loc'>" . ($lowongan['company_location'] ?: 'Location not specified') . "</p>
                         </div>
-                    </div>
+                    </a>
                 ";
             }
             
