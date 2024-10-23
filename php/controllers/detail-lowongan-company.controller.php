5ee2f0cce48db82cdbfb4e5a646bc8ff99017852
Devinzenzhang <devinzenzhang@gmail.com>
2024-10-23 20:44:19 +0700

    refactor: delete some unused files

--- php/controllers/detail-lowongan-company.controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . "/../lib/controller.php";
-class DetailLowonganCompanyController extends Controller {
-    public function handle(){
-        return $this->view("detail-lowongan-company.php");
-    }
-}
